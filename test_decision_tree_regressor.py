a5b6712a889edcf8c9c6e94ce0aadf440e66311f
Niklas Koep <niklas.koep@gmail.com>
2020-07-06 09:53:09 +0200

    Print duration of training and prediction
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- test_decision_tree_regressor.py
+++ test_decision_tree_regressor.py
@@ -1,9 +1,20 @@
+import time
+
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 
 from tree import DecisionTree
+
+
+class Timer:
+    def __enter__(self, *args, **kwargs):
+        self._start_time = time.time()
+
+    def __exit__(self, *args, **kwargs):
+        print(f"Time elapsed: {time.time() - self._start_time :.6f} seconds")
+        print()
 
 
 def main():
@@ -15,10 +26,11 @@ def main():
 
     for implementation, regressor in [("sklearn", sklearn_regressor),
                                       ("naive", naive_regressor)]:
-        regressor.fit(X_train, y_train)
-        prediction = regressor.predict(X_test)
-        print(f"Mean absolute error ({implementation}): ",
-              mean_absolute_error(prediction, y_test))
+        with Timer():
+            regressor.fit(X_train, y_train)
+            prediction = regressor.predict(X_test)
+            print(f"Mean absolute error ({implementation}):",
+                  mean_absolute_error(prediction, y_test))
 
 
 if __name__ == "__main__":
