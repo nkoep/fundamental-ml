7cd463facdaea4ff5b3ac9804e3f93052c6c9279
Niklas Koep <niklas.koep@gmail.com>
2020-08-11 18:10:52 +0200

    Finish training part of AdaBoost regressor
    Signed-off-by: Niklas Koep <niklas.koep@gmail.com>

--- boosting.py
+++ boosting.py
@@ -1,24 +1,32 @@
 import numpy as np
 from sklearn.base import BaseEstimator, RegressorMixin
 
+from random_state import ensure_random_state
 from tree import DecisionTree
 
 
-class Stump(DecisionTree):
+class Sprout(DecisionTree):
+    """A shallow decision tree regressor.
+
+    The default estimator in sklearn's AdaBoostRegressor is not a decision tree
+    stump (that is, a tree of depth 1) but a decision tree regressor of depth
+    3. We refer to this as a 'sprout' rather than a 'stump'.
+    """
+
     def __init__(self, **kwargs):
-        super().__init__(max_depth=1, **kwargs)
+        super().__init__(max_depth=3, **kwargs)
 
 
 class AdaBoost(BaseEstimator, RegressorMixin):
     def __init__(self, n_estimators=50, random_state=None):
-        self.n_estimators_ = n_estimators
-        self.random_state_ = random_state
+        self.n_estimators = n_estimators
+        self.random_state = random_state
 
-        self._reset_stumps()
+        self._reset_sprouts()
 
-    def _reset_stumps(self):
-        self._stump_weights = np.zeros(self.n_estimators_)
-        self._stumps = []
+    def _reset_sprouts(self):
+        self.sprout_weights_ = np.zeros(self.n_estimators)
+        self.sprouts_ = []
 
     def fit(self, X, y):
         """Fit the random forest to a given set of observations and targets.
@@ -38,27 +46,34 @@ class AdaBoost(BaseEstimator, RegressorMixin):
         num_samples = X.shape[0]
         sample_weights = np.ones(num_samples) / num_samples
 
-        self._reset_stumps()
+        self._reset_sprouts()
 
-        rng = np.random.default_rng(self.random_state_)
-        X_resampled, y_resampled = X, y
+        random_state = ensure_random_state(self.random_state)
 
-        for i in range(self.n_estimators_):
-            stump = Stump(random_state=self.random_state_)
-            stump.fit(X_resampled, y_resampled)
-            predictions = stump.predict(X)
-
-            # TODO: Update sample weights.
-
-            # TODO: Determine weight of the stump.
-            self._stumps.append(stump)
-
-            # Resample training set for the next iteration.
-            indices = rng.choice(
+        for i in range(self.n_estimators):
+            # Resample the training set.
+            indices = random_state.choice(
                 np.arange(num_samples), size=num_samples, replace=True,
                 p=sample_weights)
             X_resampled = X[indices, :]
-            y_resampled = y[indices, :]
+            y_resampled = y[indices]
+
+            # Train a weak learner on the resampled training data.
+            sprout = Sprout(random_state=random_state)
+            sprout.fit(X_resampled, y_resampled)
+
+            predictions = sprout.predict(X)
+            prediction_errors = np.abs(y - predictions)
+            average_loss = np.inner(
+                prediction_errors / prediction_errors.max(), sample_weights)
+
+            beta = average_loss / (1 - average_loss)
+            self.sprout_weights_[i] = np.log(1 / beta)
+            self.sprouts_.append(sprout)
+
+            # Update sample weights.
+            weights = sample_weights * beta ** (1 - average_loss)
+            sample_weights = weights / weights.sum()
 
         return self
 
@@ -75,10 +90,9 @@ class AdaBoost(BaseEstimator, RegressorMixin):
         predictions : (num_samples,) ndarray
             A vector of predictions for each individual observation.
         """
-        if not self._stumps:
+        if not self.sprouts_:
             raise RuntimeError("Estimator needs to be fitted first")
 
-        y = 0
-        for weight, stump in zip(self._stump_weights, self._stumps):
-            y += weight * stump.predict(X)
-        return y
+        predictions = np.array([
+            sprout.predict(X) for sprout in self.sprouts_])
+        return np.median(predictions, axis=0)
--- test_adaboost_regressor.py
+++ test_adaboost_regressor.py
@@ -1,18 +1,8 @@
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor as _AdaBoostRegressor
+from sklearn.ensemble import AdaBoostRegressor
 
 from example_runner import run_example
 from boosting import AdaBoost
-
-
-class AdaBoostRegressor(_AdaBoostRegressor):
-    """Convenience wrapper class to run sklearn's AdaBoostRegressor with
-    decision tree stumps of depth 1.
-    """
-
-    def __init__(self, **kwargs):
-        kwargs["base_estimator"] = DecisionTreeRegressor(max_depth=1)
-        super().__init__(**kwargs)
 
 
 regressors = [("sklearn", AdaBoostRegressor),
